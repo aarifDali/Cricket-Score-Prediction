5ec04dce57c2570a46c3583e290d7d964fa179b8
aarifDali <aliaarif995@gmail.com>
2023-10-17 12:45:13 +0530

    testing autosave

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -11,6 +11,8 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import MaxAbsScaler
+
 
 
 
